6924b9b9cab94daeff56ad56c1d3888d1daf64a3
Siddharhrabadiya <109803772+Siddharhrabadiya@users.noreply.github.com>
2022-07-26 18:30:03 +0530

    Add files via upload

---
+++ Module-1/printf.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("my name is:");
+printf("siddharth\n");
+printf("my birth date:");
+printf("23may2003\n");
+printf("my age is:");
+printf("20\n");
+printf("my address is:");
+printf("sanatan park 2,rajkot");
+getch();
+}
